eb49574196e931a0462b32daba788479178d9c4b
Lucid <bluecho97@gmail.com>
2025-05-21 22:58:07 +0900

    프로젝트 만듦

---
+++ GitStudy/GitStudy/GitStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitStudyApp.swift
+//  GitStudy
+//
+//  Created by  Lucid on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GitStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitStudy/GitStudyTests/GitStudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitStudyTests.swift
+//  GitStudyTests
+//
+//  Created by  Lucid on 5/21/25.
+//
+
+import Testing
+@testable import GitStudy
+
+struct GitStudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
